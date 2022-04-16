711c95380a4b54341516f0cf3cd79f56e426b759
Prajwal Borkar <prajwalborkar5075@gmail.com>
2022-04-16 15:35:50 +0530

    Errors resolved

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,7 @@ import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
-import AWS from '../assets/aws.png';
 import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
@@ -18,7 +16,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>// These are the technologies I've worked with</p>
+              <p className='py-4'> These are the technologies I've worked with</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
@@ -51,7 +49,7 @@ const Skills = () => {
                   <p className='my-4'>MONGO DB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
                   <p className='my-4'>AWS</p>
               </div>
           </div>
