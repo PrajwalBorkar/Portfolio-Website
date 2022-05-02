ef28958b3305a28fc307ab23fb1c3f66f78b80cf
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-02 21:09:23 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -146,7 +146,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-               
+               Keeper App
               </span>
               <div className='pt-8 text-center'>
                 <a href='https://google-keeper-app.netlify.app/' target="_blank" rel="noopener noreferrer">
