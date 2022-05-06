c1bca5d46d30df74446a1c856a2d6c2d585fed73
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-06 20:53:27 +0530

    Added Pokedex project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -141,7 +141,7 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${blog})` }}
+            style={{ backgroundImage: `url(${pokedex})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
