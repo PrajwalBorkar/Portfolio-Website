bcd8bce62c543f2eb935fe9016a97e373c38fb82
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-06 21:15:51 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a full-stack developer specializing in building exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
+          I am also an open-source enthusiast. I learned a lot from the open-source community and I love how collaboration and knowledge sharing happened through open-source.
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
