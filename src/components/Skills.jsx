fc8115cecb2e76a50ad4e2f51e2155ad13c983b5
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-06 21:06:37 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
+import java from '../assets/java.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Mongo from '../assets/mongo.png';
 
@@ -33,23 +32,23 @@ const Skills = () => {
                   <p className='my-4'>JAVASCRIPT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={java} alt="HTML icon" />
                   <p className='my-4'>REACT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
                   <p className='my-4'>GITHUB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
                   <p className='my-4'>NODE JS</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
                   <p className='my-4'>MONGO DB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
                   <p className='my-4'>Firebase</p>
               </div>
           </div>
