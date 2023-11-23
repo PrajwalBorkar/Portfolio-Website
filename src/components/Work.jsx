4b29ee35b75b4055fe02b5f4712ca0dddd5a6ebb
Prajwal <sunnyborkar7777@gmail.com>
2023-11-24 00:09:14 +0530

    Updated new projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import game from '../assets/game.png';
 import chat from '../assets/chat.png';
 import ecom from '../assets/ecommerce.png';
-import movie from '../assets/Movie.png';
 import pg from '../assets/pg.png';
 import pokedex from '../assets/pokedex.png';
+import snkrs from '../assets/SNKRS.png';
 
 const Work = () => {
   return (
@@ -47,21 +47,21 @@ const Work = () => {
           </div>
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${movie})` }}
+            style={{ backgroundImage: `url(${snkrs})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-               Movie Search Website
+               Sneakers Hub
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://moviesflix-react-app.netlify.app' target="_blank" rel="noopener noreferrer">
+                <a href='https://snkrs-hub.netlify.app/' target="_blank" rel="noopener noreferrer">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/PrajwalBorkar/movie-website' target="_blank" rel="noopener noreferrer" >
+                <a href='https://github.com/PrajwalBorkar/Sneakers-Hub' target="_blank" rel="noopener noreferrer" >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
