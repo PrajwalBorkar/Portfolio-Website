bdb7aec4e7959606af6efdb42a52a2500f5d6722
Prajwal <sunnyborkar7777@gmail.com>
2023-12-24 18:26:06 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='flex fixed flex-col  sm:hidden md:block top-[35%] left-0'>
+      <div className='flex fixed flex-col  hidden md:block top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
